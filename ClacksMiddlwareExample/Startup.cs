33887d7a7a8812e0eae8ca48a1be4384b58882fa
GaProgMan <jamie@gaprogman.com>
2017-07-22 23:28:39 +0100

    Removed unused using statments

--- ClacksMiddlwareExample/Startup.cs
+++ ClacksMiddlwareExample/Startup.cs
@@ -1,10 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using ClacksMiddleware;
 using ClacksMiddleware.Extensions;
 
 namespace ClacksMiddlwareExample
